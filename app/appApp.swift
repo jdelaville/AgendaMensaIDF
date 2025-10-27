1e4474753457144cf4df0aa040ee5748c6a0be8c
Jérôme Delaville <jdelaville@gmail.com>
2025-10-27 21:56:35 +0100

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Jérôme Delaville on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
